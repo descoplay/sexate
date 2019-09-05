412a76bf58a0b118b13f62f593c0030832cc0f9d
eudiasrafael <eu@diasrafael.com.br>
2019-09-05 16:44:59 -0300

    Refatoração do Layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -59,18 +59,34 @@ export default class App extends React.Component {
             />
         }
 
+        // return (
+        //     <div className="App">
+        //         <Layout.Row>
+        //             <Layout.Col span="4">
+        //                 <Side onTopicClick={this.changeTopic} onSearch={this.onSearch}/>
+        //             </Layout.Col>
+        //             <Layout.Col span="20">
+        //                 {content}
+        //                 <Paginate topic={this.state.topic} onChangeTopic={this.changeTopic}/>
+        //             </Layout.Col>
+        //         </Layout.Row>
+        //     </div>
+        // )
+
         return (
-            <div className="App">
-                <Layout.Row>
-                    <Layout.Col span="4">
-                        <Side onTopicClick={this.changeTopic} onSearch={this.onSearch}/>
-                    </Layout.Col>
-                    <Layout.Col span="20">
+            <Layout.Row className="App">
+                <Layout.Col span="4" className="SideArea">
+                    <Side onTopicClick={this.changeTopic} onSearch={this.onSearch}/>
+                </Layout.Col>
+                <Layout.Col span="20" className="ContentArea">
+                    <div className="TopicArea">
                         {content}
+                    </div>
+                    <div className="PaginateArea">
                         <Paginate topic={this.state.topic} onChangeTopic={this.changeTopic}/>
-                    </Layout.Col>
-                </Layout.Row>
-            </div>
+                    </div>
+                </Layout.Col>
+            </Layout.Row>
         )
     }
 }
