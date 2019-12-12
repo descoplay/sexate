73038f328927981f7cd8bfb13e0bc1abcbfcf386
eudiasrafael <eu@diasrafael.com.br>
2019-12-12 19:50:09 -0300

    TODO's

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+// TODO: Trocar alert's
+// TODO: Login/Logout com popover
+
+// TODO: Ao clicar em novo tópico, mudar URL
+// TODO: Ao criar novo tópico, mudar URL para ele
+// TODO: Ao deletar tópico, redirecionar para o primeiro tópico
+
+// TODO: Ao abrir um tópico pela busca enquanto logado, form não esta preenchendo
+
+// TODO: Ao paginar até o ultimo ou primeiro tópico, bloquear o botão de seguir ou voltar
+
+// TODO: Alinhar botões da paginação com area de login do sidebar
+// TODO: Alinhamento form login
+
+// TODO: Validação form tópicos
+
+// TODO: Componentes em formato de function
+
+// TODO: Testes unitários no backend
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
