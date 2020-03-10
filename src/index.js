6be2b6bd38cb277ffaf9161616dd7e55a80d4123
eudiasrafael <eu@diasrafael.com.br>
2020-03-10 19:13:26 -0300

    Remove todo's

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// TODO: Campo de conteúdo maior
-// TODO: Campo de tópico parente
-
-// TODO: Mudar título do sistema
-
-// TODO: Ao paginar até o ultimo ou primeiro tópico, bloquear o botão de seguir ou voltar
-
-// TODO: Alinhar botões da paginação com area de login do sidebar
-// TODO: Alinhamento form login
-
-// TODO: Validação form tópicos
-
-// TODO: Componentes em formato de function
-
-// TODO: Testes unitários no backend
-
-// TODO: Várias documentações
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
